ea33843254540e42b415c87109dd22fbe41407ab
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-08 12:09:59 +0100

    Update Featured.tsx

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -4,6 +4,8 @@ import Project from '../project/Project';
 import benna from '../../assets/benna.svg';
 import santa from '../../assets/santa.svg';
 import bay from '../../assets/bay.svg';
+import { motion as m, useAnimation } from 'framer-motion';
+import { InView } from 'react-intersection-observer';
 
 const projectData = [
   {
@@ -39,13 +41,39 @@ const projectData = [
 ];
 
 const Featured = () => {
+  const control = useAnimation();
+
+  const variants = {
+    hidden: { opacity: 0, y: 200 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleInView = (inView: any) => {
+    if (inView) {
+      control.start('visible');
+    } else {
+      control.start('hidden');
+    }
+  };
   return (
     <section className={styles.featured_container}>
-      <div className={styles.featured}>
-        <p>OVER 100 PROJECTS COMPLETED</p>
-        <h2>FEATURED PROJECTS.</h2>
-        <img src={arrow_down} alt='arrow down' />
-      </div>
+      <InView onChange={handleInView}>
+        {({ ref }) => (
+          <m.div
+            ref={ref}
+            animate={control}
+            initial='hidden'
+            variants={variants}
+            transition={{ duration: 0.5 }}
+            className={styles.featured}
+          >
+            <p>OVER 100 PROJECTS COMPLETED</p>
+            <h2>FEATURED PROJECTS.</h2>
+            <img src={arrow_down} alt='arrow down' />
+          </m.div>
+        )}
+      </InView>
       <div className={styles.project_list}>
         {projectData.map((project, idx) => (
           <Project
