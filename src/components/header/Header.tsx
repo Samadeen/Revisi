abda68a70696801e77ad6f32cddcefe8ff16f988
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-08 12:10:09 +0100

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,32 @@
 import logo from '../../assets/logo.svg';
 import menu from '../../assets/hambuger.svg';
 import styles from './Header.module.scss';
+import { motion as m } from 'framer-motion';
 
 const Header = () => {
   return (
     <header>
-      <img src={logo} alt='logo' />
-      <div className={styles.menu}>
+      <m.img
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          duration: 0.9,
+          type: 'spring',
+          stiffness: 200,
+          ease: 'linear',
+        }}
+        src={logo}
+        alt='logo'
+      />
+      <m.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}
+        className={styles.menu}
+      >
         <img src={menu} alt='hamburger' />
         <p>Menu</p>
-      </div>
+      </m.div>
     </header>
   );
 };
