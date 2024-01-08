db5b868434e180998ac7224f27344fd7cfe8261c
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-08 12:10:32 +0100

    Update Possible.tsx

--- src/components/possible/Possible.tsx
+++ src/components/possible/Possible.tsx
@@ -1,20 +1,26 @@
 import Button from '../button/Button';
 import styles from './Possible.module.scss';
 import bed from '../../assets/possible.svg';
+import { motion as m } from 'framer-motion';
 
 const Possible = () => {
   return (
-    <section className={styles.possible_container}>
-      <div className={styles.possible_text}>
+    <m.section
+      initial={{ y: 200, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 2, type: 'spring' }}
+      className={styles.possible_container}
+    >
+      <m.div className={styles.possible_text}>
         <h2>WE MAKE EVERYTHING POSSIBLE.</h2>
         <p>
           We are proud of our expertise. Everything design and its complexities
           are a drive for us to provide better services and top notch designs.
         </p>
         <Button color='wine' text='Learn More' />
-      </div>
-      <img src={bed} alt='bed in view' />
-    </section>
+      </m.div>
+      <m.img src={bed} alt='bed in view' />
+    </m.section>
   );
 };
 
