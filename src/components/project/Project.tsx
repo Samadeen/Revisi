16e65dd431f71db544815857b70ef2d7430619ea
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-08 12:10:40 +0100

    Update Project.tsx

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,5 +1,6 @@
 import Button from '../button/Button';
 import styles from './Project.module.scss';
+import { motion as m } from 'framer-motion';
 
 interface ProjectProps {
   title: string;
@@ -13,18 +14,23 @@ const Project = ({ title, note1, note2, url }: ProjectProps) => {
     <div className={styles.project_container}>
       <div className={styles.image_container}>
         <div className={styles.image_overlay}>
-          <img src={url} alt='' />
+          <img src={url} alt={title} />
           <div className={styles.text}>
             <h3>{title}</h3>
             <Button color='white' text='Explore' />
           </div>
         </div>
       </div>
-      <div className={styles.details_container}>
+      <m.div
+        initial={{ y: 100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: 0.2, type: 'spring' }}
+        className={styles.details_container}
+      >
         <h2>{title}</h2>
         <p className={styles.note1}>{note1}</p>
         <p className={styles.note2}>{note2}</p>
-      </div>
+      </m.div>
     </div>
   );
 };
