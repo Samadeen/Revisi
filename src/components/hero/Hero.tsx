36f37233ed04432e6318049ace86d6fc28e630b0
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-08 12:10:16 +0100

    Update Hero.tsx

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,15 +6,25 @@ const Hero = () => {
   return (
     <section className={styles.hero_container}>
       <div className={styles.hero_details}>
-        <h1>EXPLORE THE IMPOSSIBLE.</h1>
+        <m.h1
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.3, ease: 'anticipate' }}
+        >
+          EXPLORE THE IMPOSSIBLE.
+        </m.h1>
         <div className={styles.para}>
-          <p>
+          <m.p
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.1, delay: 0.5, ease: 'anticipate' }}
+          >
             Founders turn to us when failure is not an option. Our reputation is
             built on our design prowess and rapid execution abilities. Yet, more
             often than not, it's the recommendations of those they trust that
             lead them to us. Founders turn to us when failure is not an option.
             Our reputation is built on our design prowess and rapid.
-          </p>
+          </m.p>
           <Button text='BOOK CONSULTATION' color='wine' />
         </div>
       </div>
